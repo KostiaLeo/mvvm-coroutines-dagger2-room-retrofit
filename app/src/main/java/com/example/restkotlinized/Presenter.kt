c35e286181808e6fdb07be0f6e9d182e52bd505e
Konstantin Levitskiy <44975978+KostiaLeo@users.noreply.github.com>
2019-11-22 23:02:09 +0200

    Full mvp supporting, comfortable Rx using without statics initializations

--- app/src/main/java/com/example/restkotlinized/Presenter.kt
+++ app/src/main/java/com/example/restkotlinized/Presenter.kt
@@ -1,26 +1,10 @@
 package com.example.restkotlinized
 
 import com.example.restkotlinized.model.Results
-import io.reactivex.Observable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
-import io.reactivex.subjects.BehaviorSubject
 
 class Presenter(private val artistsView: ArtistsContract.View) : ArtistsContract.Presenter, ArtistsContract.Model.OnFinishedListener {
 
-    companion object {
-        private val loadSubject = BehaviorSubject.create<List<Results>>()
-        val loadObservable: Observable<List<Results>> = loadSubject.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
-
-    }
-
     private var artistsModel: ArtistsModel = ArtistsModel()
-
-    //private var artistsView: ArtistsContract.View = artistsView
-
-//    init {
-//        artistsModel = ArtistsModel()
-//    }
 
     override fun requestDataFromServer() {
         artistsModel.getArtistsList(this)
@@ -28,7 +12,6 @@ class Presenter(private val artistsView: ArtistsContract.View) : ArtistsContract
     }
 
     override fun onFinished(results: List<Results>) {
-        loadSubject.onNext(results)
         artistsView.hideProgress()
         artistsView.setDataToRecyclerView(results)
     }
--- app/src/main/java/com/example/restkotlinized/view/fragments/StoriesFragment.kt
+++ app/src/main/java/com/example/restkotlinized/view/fragments/StoriesFragment.kt
@@ -21,7 +21,14 @@ import com.example.restkotlinized.ShowEmptyView
 import com.example.restkotlinized.model.Results
 import com.example.restkotlinized.view.fragments.mainAdapter.NewzAdapter
 import com.example.restkotlinized.view.fragments.topAdapter.TopNewzAdapter
-
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.Subject
+import java.util.*
+import kotlin.collections.ArrayList
 
 class StoriesFragment(context: Context) : Fragment(), ArtistsContract.View, ShowEmptyView {
 
@@ -30,64 +37,123 @@ class StoriesFragment(context: Context) : Fragment(), ArtistsContract.View, Show
     private var progressBar: ProgressBar? = null
     private var emptyView: TextView? = null
 
+    private var root: View = View(ctx)
+
     private var presenter: Presenter = Presenter(this)
 
     private var allResults: ArrayList<Results>? = null
 
     private var mainAdapter: NewzAdapter? = null
-    private var adapterForVP: TopNewzAdapter? = null
+    private var adapterForTopNewz: TopNewzAdapter? = null
+
+    private val loadSubject = BehaviorSubject.create<List<Results>>()
+    private val loadObservable
+            = loadSubject.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
+    private var disposableLoader: Disposable? = null
+
 
     companion object Factory {
         fun create(context: Context): StoriesFragment =
             StoriesFragment(context)
     }
 
-    @SuppressLint("CheckResult")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_stories, container, false)
+        this.root = root
+
         presenter.requestDataFromServer()
 
-        Presenter.loadObservable.subscribe {
-            results -> initUI(root, ArrayList(results))
-        }
+        //initUI(observeResult())
+        observeResult()
 
         return root
     }
 
-    private fun initUI(root: View, allResults: ArrayList<Results>){
+    @SuppressLint("CheckResult")
+    fun observeResult() {
+        disposableLoader = loadObservable.subscribe { results ->
+            putResultsToInitUI(results)
+        }
+    }
+
+    private fun putResultsToInitUI(r: List<Results>){
+        initUI(ArrayList(r))
+    }
+
+    private fun initUI(allResults: ArrayList<Results>) {
         println("initUI(): allResults size = ${allResults.size}")
 
         mainAdapter = NewzAdapter(allResults)
-        adapterForVP = TopNewzAdapter(allResults)
+        adapterForTopNewz = TopNewzAdapter(allResults)
 
         val newsRecycler = root.findViewById<RecyclerView>(R.id.newzzz)
-        newsRecycler.layoutManager = LinearLayoutManager(ctx)
-        newsRecycler.itemAnimator = DefaultItemAnimator()
+        newsRecycler?.layoutManager = LinearLayoutManager(ctx)
+        newsRecycler?.itemAnimator = DefaultItemAnimator()
         progressBar = root.findViewById<ProgressBar>(R.id.pb_loading)
 
         val viewPager = root.findViewById<ViewPager2>(R.id.viewPager2)
-        viewPager.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        viewPager?.orientation = ViewPager2.ORIENTATION_HORIZONTAL
 
         val sliderDotsPanel = root.findViewById<LinearLayout>(R.id.SliderDots)
         setDots(sliderDotsPanel, viewPager)
 
         newsRecycler?.adapter = mainAdapter
-        viewPager?.adapter = adapterForVP
+        viewPager?.adapter = adapterForTopNewz
+    }
+
+    private fun setDots(sliderDotsPanel: LinearLayout, viewPager: ViewPager2) {
+
+        sliderDotsPanel.bringToFront()
+        val dotsCount = TopNewzAdapter.AMOUNT_OF_TOPNEWS
+
+        val dots = arrayOfNulls<ImageView>(dotsCount)
+
+        for (i in 1..dotsCount) {
+            dots[(i - 1)] = ImageView(ctx)
+        }
+
+        dots.forEach {
+
+            it?.setImageDrawable(ContextCompat.getDrawable(ctx, R.drawable.dotgrey))
+            val params = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            params.setMargins(8, 0, 8, 0)
+            params.gravity = Gravity.CENTER
+            sliderDotsPanel.addView(it, params)
+        }
+
+        dots[0]?.setImageDrawable(ContextCompat.getDrawable(ctx, R.drawable.dotblue))
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+//                super.onPageSelected(position)
+                dots.forEach {
+                    it?.setImageDrawable(ContextCompat.getDrawable(ctx, R.drawable.dotgrey))
+                    //val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+                }
+                dots[position]?.setImageDrawable(ContextCompat.getDrawable(ctx, R.drawable.dotblue))
+            }
+        })
     }
 
     override fun setDataToRecyclerView(results: List<Results>) {
-            val res: ArrayList<Results> = ArrayList(results)
-            println("setting adapter, res size = ${res.size}")
-            allResults?.addAll(res)
-            mainAdapter?.notifyDataSetChanged()
-    }
+        val res: ArrayList<Results> = ArrayList(results)
+        println("setting adapter, res size = ${res.size}")
 
+        loadSubject.onNext(results)
+//        allResults?.addAll(res)
+        mainAdapter?.notifyDataSetChanged()
+
+    }
+//    fun List<Results>.sendOnNext(loadSubject: Subject<Results>){
+//        this.forEach {
+//            loadSubject.onNext(it)
+//        }
+//    }
     override fun onResponseFailure(throwable: Throwable) {
-        Toast.makeText(
-            context,
-            "Сервер не отвечает, попробуйте позже",
-            Toast.LENGTH_SHORT
-        ).show()
+        Toast.makeText(context, "Сервер не отвечает, попробуйте позже", Toast.LENGTH_SHORT).show()
         println(throwable)
     }
 
@@ -110,44 +176,11 @@ class StoriesFragment(context: Context) : Fragment(), ArtistsContract.View, Show
     }
 
 
-// ------------------ UI ------------------
-    private fun setDots(sliderDotsPanel: LinearLayout, viewPager: ViewPager2){
-
-        sliderDotsPanel.bringToFront()
-        val dotsCount = TopNewzAdapter.AMOUNT_OF_TOPNEWS
-
-        val dots = arrayOfNulls<ImageView>(dotsCount)
-
-        for(i in 1..dotsCount){
-            dots[(i-1)] = ImageView(ctx)
-        }
-
-        dots.forEach {
-
-            it?.setImageDrawable(ContextCompat.getDrawable(ctx, R.drawable.dotgrey))
-            val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-            params.setMargins(8, 0, 8, 0)
-            params.gravity = Gravity.CENTER
-            sliderDotsPanel.addView(it, params)
-        }
-
-        dots[0]?.setImageDrawable(ContextCompat.getDrawable(ctx, R.drawable.dotblue))
-
-        viewPager.registerOnPageChangeCallback(object: ViewPager2.OnPageChangeCallback(){
-            override fun onPageSelected(position: Int) {
-//                super.onPageSelected(position)
-                dots.forEach {
-                    it?.setImageDrawable(ContextCompat.getDrawable(ctx, R.drawable.dotgrey))
-                    //val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-                }
-                dots[position]?.setImageDrawable(ContextCompat.getDrawable(ctx, R.drawable.dotblue))
-            }
-        })
-    }
+    // ------------------ UI ------------------
 
 
     override fun onDestroyView() {
         super.onDestroyView()
-
+        disposableLoader?.dispose()
     }
 }
