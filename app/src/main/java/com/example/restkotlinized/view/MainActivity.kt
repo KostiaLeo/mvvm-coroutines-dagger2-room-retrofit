d3a110dc7af495eadc780925484e011c96791a3a
Konstantin Levitskiy <44975978+KostiaLeo@users.noreply.github.com>
2019-11-23 00:21:25 +0200

    Some refactoring

--- app/src/main/java/com/example/restkotlinized/view/MainActivity.kt
+++ app/src/main/java/com/example/restkotlinized/view/MainActivity.kt
@@ -14,10 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sectionPagerAdapter = SectionPagerAdapter(
-            this,
-            supportFragmentManager
-        )
+        val sectionPagerAdapter = SectionPagerAdapter(this, supportFragmentManager)
 
         val viewPager: ViewPager = findViewById(R.id.view_pager)
         viewPager.adapter = sectionPagerAdapter
